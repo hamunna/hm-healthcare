2ee599b5d546fd27b707f53b87bb473cdd57bae8
hamunna <hamunna@gmail.com>
2021-10-18 01:57:11 +0600

    Banner Slider Created

--- src/Pages/HomePage/About/About.js
+++ src/Pages/HomePage/About/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const About = () => {
 	return (
 		<div>
-			<h2>This is About Section</h2>
+			<Container>
+				<div className="d-flex">
+					<div>
+						<h5 className="text-uppercase text-secondary fw-bolder">Aout us</h5>
+						<h2 className="text-uppercase">the world's best treatment in our hospital</h2>
+
+						<h5 className="theme-primary-text">Health services consist of medical professionals, organizations, and ancillary health</h5>
+
+						<p className="text-secondary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa hic sint ex, officia autem magni amet sit explicabo molestias quod deleniti consequuntur deserunt nihil corrupti perferendis temporibus nam error soluta.</p>
+
+					</div>
+					<div></div>
+				</div>
+			</Container>
 		</div>
 	);
 };
--- src/Pages/HomePage/Home/Home.js
+++ src/Pages/HomePage/Home/Home.js
@@ -1,61 +1,64 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 
-import slider1 from '../../../../public/images/banners/slider-1.png';
-import slider2 from '../../../../public/images/banners/slider-2.jpg';
-import slider3 from '../../../../public/images/banners/slider-3.jpg';
+import slider1 from '../../../images/banners/slider-1.png';
+import slider2 from '../../../images/banners/slider-2.jpg';
+import slider3 from '../../../images/banners/slider-3.jpg';
 
 const Home = () => {
 	return (
-		<div>
-			<Carousel fade>
+		<main>
 
-				{/* Banner Slider-1 */}
-				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src={slider1}
-						alt="First slide"
-					/>
-					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
+			{/* Banner Slider Start */}
+			<div>
+				<Carousel fade variant="dark">
 
-
-				{/* Banner Slider-2 */}
-				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src={slider2}
-						alt="Second slide"
-					/>
-
-					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
+					{/* Banner Slider-1 */}
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src={slider1}
+							alt="First slide"
+						/>
+						<Carousel.Caption>
+							<h3>Feel better soon!</h3>
+							<p>May good health envelop you, spurring a quick recovery.</p>
+						</Carousel.Caption>
+					</Carousel.Item>
 
 
-				{/* Banner Slider-3 */}
-				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src={slider3}
-						alt="Third slide"
-					/>
+					{/* Banner Slider-2 */}
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src={slider2}
+							alt="Second slide"
+						/>
 
-					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
+						<Carousel.Caption>
+							<h3>Have a speedy recovery!</h3>
+							<p>We hope each new day brings you closer to a full and speedy recovery!</p>
+						</Carousel.Caption>
+					</Carousel.Item>
 
 
-			</Carousel>
-		</div>
+					{/* Banner Slider-3 */}
+					<Carousel.Item>
+						<img
+							className="d-block w-100"
+							src={slider3}
+							alt="Third slide"
+						/>
+
+						<Carousel.Caption>
+							<h3>Hoping you find strength with each new day</h3>
+							<p>Thinking of you lots and hoping for your speedy recovery.</p>
+						</Carousel.Caption>
+					</Carousel.Item>
+				</Carousel>
+			</div>
+			{/* Banner Slider END */}
+		</main>
 	);
 };
 
